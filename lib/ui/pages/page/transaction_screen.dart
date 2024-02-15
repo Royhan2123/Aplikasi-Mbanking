9949e7715611656c11a56c17582ed35b71593224
Royhan <royhankeren12@gmail.com>
2024-02-15 23:09:41 +0700

    new Folder

---
+++ lib/ui/pages/page/transaction_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class TransactionScreen extends StatefulWidget {
+  const TransactionScreen({super.key});
+
+  @override
+  State<TransactionScreen> createState() => _TransactionScreenState();
+}
+
+class _TransactionScreenState extends State<TransactionScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: [
+          Text("Transaction")
+        ],
+      ),
+    );
+  }
+}
