acfc04dcb6a75ee3955c1a00fb3e76a4da7ddfa1
Royhan <royhankeren12@gmail.com>
2024-02-16 20:31:31 +0700

    add headher

--- lib/ui/pages/page/home_screen.dart
+++ lib/ui/pages/page/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:aplikasi_mbanking/style/color/style_color.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -12,8 +13,64 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: ListView(
+        padding: const EdgeInsets.all(
+          20,
+        ),
         children: [
-          Text("HOME")
+          headher(),
+        ],
+      ),
+    );
+  }
+
+  Widget headher() {
+    return Container(
+      margin: const EdgeInsets.only(
+        top: 50,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Hello,",
+                style: blackStyle.copyWith(
+                  fontSize: 15,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              Text(
+                "Mr. Alexander Francis",
+                style: blackStyle.copyWith(
+                  fontSize: 17,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              Text(
+                "How are you today?",
+                style: greyStyle.copyWith(
+                  fontSize: 17,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ],
+          ),
+          Container(
+            width: 80,
+            height: 80,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(
+                20,
+              ),
+              color: const Color(0xffDAB66F),
+              image: const DecorationImage(
+                image: AssetImage("assets/images/avatar.png"),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
         ],
       ),
     );
