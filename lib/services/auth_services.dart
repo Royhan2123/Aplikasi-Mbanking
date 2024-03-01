54ff407fb412694aba211ed0e4066baa1717d63c
Royhan <royhankeren12@gmail.com>
2024-03-01 21:49:29 +0700

    new package dio

---
+++ lib/services/auth_services.dart
@@ -0,0 +1,13 @@
+import 'package:aplikasi_mbanking/models/signUp_model.dart';
+
+class AuthServices {
+  String baseUrl = "https://bwabank.my.id/api/";
+
+  Future<SignUpModels> checkEmail(String email) async {
+    try {
+      final response = await 
+    } catch (e) {
+      throw e.toString();
+    }
+  }
+}
