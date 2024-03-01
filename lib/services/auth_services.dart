df1a777e27a15de70664ce54238d2f7e2e6abf58
Royhan <royhankeren12@gmail.com>
2024-03-01 22:37:31 +0700

    auth services with dio

--- lib/services/auth_services.dart
+++ lib/services/auth_services.dart
@@ -1,11 +1,27 @@
 import 'package:aplikasi_mbanking/models/signUp_model.dart';
+import 'package:dio/dio.dart';
 
 class AuthServices {
   String baseUrl = "https://bwabank.my.id/api/";
+  final dio = Dio();
 
   Future<SignUpModels> checkEmail(String email) async {
     try {
-      final response = await 
+      final response = await dio.post(
+        "$baseUrl/is-email-exist",
+        data: {
+          "email": email,
+        },
+      );
+
+      // ignore: avoid_print
+      print(response.data.toString());
+
+      if (response.statusCode == 200) {
+        return response.data["is_email_exist"];
+      } else {
+        return response.data["errors"];
+      }
     } catch (e) {
       throw e.toString();
     }
