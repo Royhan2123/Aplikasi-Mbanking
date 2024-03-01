f22c3e641a8a394c7d343487495132416114c3e6
Royhan <royhankeren12@gmail.com>
2024-03-01 21:36:08 +0700

    signIn Models

---
+++ lib/models/signin_model.dart
@@ -0,0 +1,16 @@
+class SignInModel {
+  final String? email;
+  final String? password;
+
+  const SignInModel({
+    required this.email,
+    required this.password,
+  });
+
+  Map<String, dynamic> toJson() => {
+        "email": email,
+        "password": password,
+      };
+
+      
+}
