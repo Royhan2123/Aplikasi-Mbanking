10a3e9deb4b7c5bc4624bb6f55158e13ffad945b
Royhan <royhankeren12@gmail.com>
2024-07-02 23:36:46 +0700

    connect auth bloc -register in signUp photo & register screen

--- lib/ui/pages/signup_profile_photo_screen.dart
+++ lib/ui/pages/signup_profile_photo_screen.dart
@@ -64,18 +64,7 @@ class _SignUpProfilePhotoScreenState extends State<SignUpProfilePhotoScreen> {
       ),
       body: ListView(
         children: [
-          inputImage(),
-          inputPin(),
-        ],
-      ),
-    );
-  }
-
-  Widget inputImage() {
-    return BlocBuilder<AuthBloc, AuthState>(
-      builder: (context, state) {
-        if (state is AuthSucces) {
-          return Center(
+          Center(
             child: Column(
               children: [
                 const SizedBox(
@@ -144,7 +133,7 @@ class _SignUpProfilePhotoScreenState extends State<SignUpProfilePhotoScreen> {
                       ),
                       Center(
                         child: Text(
-                          state.user.name.toString(),
+                          widget.data.name.toString(),
                           style: blackStyle.copyWith(
                             fontSize: 16,
                           ),
@@ -155,128 +144,68 @@ class _SignUpProfilePhotoScreenState extends State<SignUpProfilePhotoScreen> {
                 ),
               ],
             ),
-          );
-        }
-        return const Center(
-          child: CircularProgressIndicator(),
-        );
-      },
-    );
-  }
-
-  Widget inputPin() {
-    return Column(
-      children: [
-        const SizedBox(
-          height: 25,
-        ),
-        Text(
-          "Set your pin",
-          style: blackStyle.copyWith(
-            fontSize: 14,
           ),
-        ),
-        const SizedBox(
-          height: 20,
-        ),
-        TextFormField(
-          controller: txtPin,
-          inputFormatters: [
-            LengthLimitingTextInputFormatter(6),
-          ],
-          style: const TextStyle(
-            color: Colors.black,
-            fontSize: 15,
-          ),
-          decoration: InputDecoration(
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(10),
-            ),
-            contentPadding: const EdgeInsets.all(10),
-            hintText: "enter your pin 6 digit",
-            hintStyle: const TextStyle(
-              color: Colors.grey,
-            ),
-            focusedBorder: OutlineInputBorder(
-              borderSide: BorderSide(
-                color: blues,
-                width: 1.5,
+          Column(
+            children: [
+              const SizedBox(
+                height: 25,
               ),
-              borderRadius: BorderRadius.circular(10),
-            ),
-            enabledBorder: OutlineInputBorder(
-              borderSide: const BorderSide(
-                color: Colors.black,
-              ),
-              borderRadius: BorderRadius.circular(10),
-            ),
-          ),
-          obscureText: true,
-          keyboardType: TextInputType.number,
-        ),
-        const SizedBox(
-          height: 20,
-        ),
-        Center(
-          child: BlocConsumer<AuthBloc, AuthState>(
-            listener: (context, state) {
-              if (state is AuthSucces) {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => SignUpProfileVerifyScreen(
-                      data: widget.data.copyWith(
-                        pin: txtPin.text,
-                        profilePicture: selectedImage == null
-                            ? null
-                            // ignore: prefer_interpolation_to_compose_strings
-                            : 'data:image/png;base64,' +
-                                base64Encode(
-                                  File(selectedImage!.path).readAsBytesSync(),
-                                ),
-                      ),
-                    ),
-                  ),
-                );
-              } else if (state is AuthFailed) {
-                showCustomSnackbar(
-                  context,
-                  state.e,
-                );
-              }
-            },
-            builder: (context, state) {
-              return ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  animationDuration: const Duration(
-                    seconds: 3,
-                  ),
-                  backgroundColor: blues,
-                  minimumSize: const Size(
-                    350,
-                    35,
-                  ),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(
-                      15,
-                    ),
-                  ),
-                  foregroundColor: primary,
+              Text(
+                "Set your pin",
+                style: blackStyle.copyWith(
+                  fontSize: 14,
                 ),
-                onPressed: () {
-                  if (txtPin.text.isEmpty) {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(
-                        content: Text(
-                          "Tolong Masukkan Pin Anda",
-                        ),
-                        backgroundColor: Colors.red,
-                      ),
-                    );
-                  } else {
-                    context.read<AuthBloc>().add(
-                          AuthRegister(
-                            widget.data.copyWith(
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                controller: txtPin,
+                inputFormatters: [
+                  LengthLimitingTextInputFormatter(6),
+                ],
+                style: const TextStyle(
+                  color: Colors.black,
+                  fontSize: 15,
+                ),
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  contentPadding: const EdgeInsets.all(10),
+                  hintText: "enter your pin 6 digit",
+                  hintStyle: const TextStyle(
+                    color: Colors.grey,
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: blues,
+                      width: 1.5,
+                    ),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: const BorderSide(
+                      color: Colors.black,
+                    ),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                obscureText: true,
+                keyboardType: TextInputType.number,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Center(
+                child: BlocConsumer<AuthBloc, AuthState>(
+                  listener: (context, state) {
+                    if (state is AuthSucces) {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => SignUpProfileVerifyScreen(
+                            data: widget.data.copyWith(
                               pin: txtPin.text,
                               profilePicture: selectedImage == null
                                   ? null
@@ -288,21 +217,76 @@ class _SignUpProfilePhotoScreenState extends State<SignUpProfilePhotoScreen> {
                                       ),
                             ),
                           ),
-                        );
-                  }
-                },
-                child: const Text(
-                  "Continue",
-                  style: TextStyle(color: Colors.white),
+                        ),
+                      );
+                    } else if (state is AuthFailed) {
+                      showCustomSnackbar(
+                        context,
+                        state.e,
+                      );
+                    }
+                  },
+                  builder: (context, state) {
+                    return ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        animationDuration: const Duration(
+                          seconds: 3,
+                        ),
+                        backgroundColor: blues,
+                        minimumSize: const Size(
+                          350,
+                          35,
+                        ),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(
+                            15,
+                          ),
+                        ),
+                        foregroundColor: primary,
+                      ),
+                      onPressed: () {
+                        if (txtPin.text.isEmpty) {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                              content: Text(
+                                "Tolong Masukkan Pin Anda",
+                              ),
+                              backgroundColor: Colors.red,
+                            ),
+                          );
+                        } else {
+                          context.read<AuthBloc>().add(
+                                AuthRegister(
+                                  widget.data.copyWith(
+                                    pin: txtPin.text,
+                                    profilePicture: selectedImage == null
+                                        ? null
+                                        // ignore: prefer_interpolation_to_compose_strings
+                                        : 'data:image/png;base64,' +
+                                            base64Encode(
+                                              File(selectedImage!.path)
+                                                  .readAsBytesSync(),
+                                            ),
+                                  ),
+                                ),
+                              );
+                        }
+                      },
+                      child: const Text(
+                        "Continue",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    );
+                  },
                 ),
-              );
-            },
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+            ],
           ),
-        ),
-        const SizedBox(
-          height: 10,
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
